75e42db0cc51cb15ddcd5acb97cccaef8e70ab95
Nick Williams <git@nickawilliams.com>
2024-05-13 12:42:42 -0700

    chore(uhura-service): made port configurable

--- uhura-service/src/main.ts
+++ uhura-service/src/main.ts
@@ -1,10 +1,15 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './modules/app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    snapshot: true,
-  })
-  await app.listen(3000)
+async function main() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const config = app.get(ConfigService)
+
+  app.disable('x-powered-by')
+
+  await app.listen(config.get('SERVICE_PORT') || 3000)
 }
-bootstrap()
+
+main()
