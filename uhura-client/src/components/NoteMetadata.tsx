893a269e46474a7cef324236b72de5bcc8f0e375
Nick Williams <git@nickawilliams.com>
2024-05-17 12:48:46 -0700

    feat(uhura-client): created a note metadata component

---
+++ uhura-client/src/components/NoteMetadata.tsx
@@ -0,0 +1,20 @@
+import { Badge, Text } from '@mantine/core'
+import { Note } from '../common/types'
+import { NoteUtils } from '../lib/note.utils'
+
+export const NoteMetadata = ({ note }: { note: Note }) => {
+  const { address } = note
+  const meta = NoteUtils.getMetadataText(note)
+  const url = NoteUtils.getAddressUrl(note)
+
+  return (
+    <Text c="dark" fw={700}>
+      {meta}
+      <a href={url}>
+        <Badge color="blue" variant="filled" tt="inherit">
+          {address}
+        </Badge>
+      </a>
+    </Text>
+  )
+}
