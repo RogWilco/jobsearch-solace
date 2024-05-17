3c6cddaaef53fc2e21227669f6ce5893456f75a4
Nick Williams <git@nickawilliams.com>
2024-05-16 20:36:09 -0700

    feat(uhura-service): added note service for interacting with the uhura-service API

--- uhura-client/src/common/types.ts
+++ uhura-client/src/common/types.ts
@@ -1,10 +1,11 @@
 export type Note = {
   id: string
   title: string
-  content: string // markdown
+  content: string
   type: 'call' | 'fax' | 'email' | 'mail' | 'meeting' | 'submission' | 'other'
   address: string
   direction?: 'inbound' | 'outbound'
+  status: 'success' | 'pending' | 'failed'
   created: string
   updated: string
 }
--- uhura-client/src/lib/note.service.ts
+++ uhura-client/src/lib/note.service.ts
@@ -1,11 +1,98 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
+import { Note } from '../common/types'
 
+/**
+ * A service for interacting with notes.
+ */
 export class NoteService {
-  constructor(
-    private readonly _baseUrl: string = 'http://localhost:3000/notes',
-  ) {}
+  /**
+   * The singleton instance of the note service.
+   */
+  private static _instance: NoteService
 
-  public async getNotes(): Promise<AxiosResponse> {
-    return axios.get(this._baseUrl)
+  /**
+   * Fetches the singleton instance of the note service.
+   */
+  public static get instance(): NoteService {
+    if (!this._instance) {
+      this._instance = new NoteService(import.meta.env.VITE_API_URL + '/notes')
+    }
+
+    return this._instance
+  }
+
+  /**
+   * Initializes a new note service.
+   *
+   * @param _baseUrl the base url of the note service
+   */
+  constructor(private readonly _baseUrl: string) {}
+
+  /**
+   * Fetches all notes.
+   *
+   * @returns an array of notes
+   */
+  public async getMany(): Promise<Note[]> {
+    const res = await axios.get<Note[]>(this._baseUrl, {
+      headers: { Accept: 'application/json' },
+    })
+
+    return res.data
+  }
+
+  /**
+   * Fetches a single note by id.
+   *
+   * @param id the id of the note to fetch
+   *
+   * @returns the corresponding note
+   */
+  public async getOne(id: string): Promise<Note> {
+    const res = await axios.get<Note>(`${this._baseUrl}/${id}`, {
+      headers: { Accept: 'application/json' },
+    })
+
+    return res.data
+  }
+
+  /**
+   * Creates a new note.
+   *
+   * @param note the note to create
+   *
+   * @returns the created note
+   */
+  public async create(note: Note): Promise<Note> {
+    const res = await axios.post<Note>(this._baseUrl, note, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    return res.data
+  }
+
+  /**
+   * Updates an existing note by its id.
+   *
+   * @param id the id of the note to update
+   * @param note the data with which to update the note
+   *
+   * @returns the updated note
+   */
+  public async update(id: Note['id'], note: Partial<Note>): Promise<Note> {
+    const res = await axios.put<Note>(`${this._baseUrl}/${id}`, note, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    return res.data
+  }
+
+  /**
+   * Deletes a note by its id.
+   *
+   * @param id the id of the note to delete
+   */
+  public async delete(id: string): Promise<void> {
+    await axios.delete(`${this._baseUrl}/${id}`)
   }
 }
